d14f17e42ccebf2283d6566994e4504a1b370b17
valinurovdenis <denis.valinurow@yandex.ru>
2018-04-22 14:25:40 +0300

    HW6: init

--- src/coroutine/Engine.cpp
+++ src/coroutine/Engine.cpp
@@ -3,17 +3,64 @@
 #include <setjmp.h>
 #include <stdio.h>
 #include <string.h>
+#include <cstdlib>
 
 namespace Afina {
 namespace Coroutine {
 
-void Engine::Store(context &ctx) {}
+void Engine::Store(context &ctx) {
+    char this_address;
 
-void Engine::Restore(context &ctx) {}
+    ctx.Low = &this_address;
+    ctx.Hight = StackBottom;
 
-void Engine::yield() {}
+    uint32_t size = uint32_t(abs(&this_address - StackBottom));
+    if ((std::get<0>(ctx.Stack) == nullptr) || (size > std::get<1>(ctx.Stack))) {
+        free(std::get<0>(ctx.Stack));
+        std::get<0>(ctx.Stack) = (char *)malloc(sizeof(char) * size);
+        std::get<1>(ctx.Stack) = size;
+    }
 
-void Engine::sched(void *routine_) {}
+    memcpy(std::get<0>(ctx.Stack), &this_address, std::get<1>(ctx.Stack));
+}
+
+void Engine::Restore(context &ctx) {
+    char this_address;
+
+    if (abs(&this_address - StackBottom) < std::get<1>(ctx.Stack)) {
+        Restore(ctx);
+    }
+    memcpy(ctx.Low, std::get<0>(ctx.Stack), std::get<1>(ctx.Stack));
+    longjmp(ctx.Environment, 1);
+}
+
+void Engine::yield() {
+    context *next_routine = alive;
+    if (next_routine == cur_routine && next_routine != nullptr){
+        next_routine = next_routine->next;
+    }
+
+    if(next_routine != nullptr) {
+        sched(next_routine);
+    }
+}
+
+void Engine::sched(void *routine_) {
+    context *called = reinterpret_cast<context *>(routine_);
+
+    if (called && called != cur_routine) {
+        if (cur_routine) {
+            Store(*cur_routine);
+            if (setjmp(cur_routine->Environment) > 0) {
+                return;
+            }
+        }
+        cur_routine = called;
+        Restore(*called);
+    } else {
+        yield();
+    }
+}
 
 } // namespace Coroutine
 } // namespace Afina
